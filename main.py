25f466632148d9f0b84bf4c96be60a0cc0d57d3c
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-03 21:34:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+states_Of_America = ["Alabama", "Alaska", "Arizona", "Arkansas", "California" , "Colorado", "Connecticut", "Delaware", "Florida", "Georgia" , "Hawaii", "Idaho", "Illinois", "Indiana",  ]
+print (states_Of_America [1])
+
+states_Of_America[1] = "alasska"
+
+
+print(states_Of_America[1])
+
+states_Of_America.extend(["Xyz"])
+print(states_Of_America)
